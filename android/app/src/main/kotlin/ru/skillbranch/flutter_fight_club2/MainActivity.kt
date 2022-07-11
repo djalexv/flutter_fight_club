0874cece69e1888dae5252493da66b2df03806e0
Alex <alex.4soft@mail.ru>
2022-07-11 20:56:38 +0700

    000000

---
+++ android/app/src/main/kotlin/ru/skillbranch/flutter_fight_club2/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.skillbranch.flutter_fight_club2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_00.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          SizedBox(
+            height: 40,
+            width: 158,
+            child: ColoredBox(
+              color: Colors.black26,
+              child: Text("HEAD"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
